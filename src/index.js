03222eff1b1029083ddb41b2a59da16028ea71e5
Peyton Bechard <pbechard@my.yorku.ca>
2022-06-05 19:34:35 +0800

    Adds HTML elements and basic styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import './style.css';
-// import favicon from './img/umbrella.svg';
 
-// // const fav = new Image();
-// // fav.src = favicon;
-// document.
+// https://api.openweathermap.org/data/2.5/weather?q=Shenyang&appid=d35c7255a79efc255f423d8ee7ce896b
